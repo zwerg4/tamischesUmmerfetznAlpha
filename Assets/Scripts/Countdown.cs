8f8198e737ca4463e79cb805b552edf6edd0345a
fabio <fabio.gandolfi@student.tugraz.at>
2020-06-01 16:21:05 +0200

    cloud computing via unity cloud

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+
+    public GameObject Countdown_;
+    public AudioSource getReady;
+    public AudioSource goAudio;
+    public GameObject LapTimer;
+    public GameObject CarControls;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    	StartCoroutine(CountStart());    
+    }
+    
+    IEnumerator CountStart()
+    {
+    	yield return new WaitForSeconds(0.5f);
+    	Countdown_.GetComponent<Text> ().text = "3";
+    	getReady.Play();
+    	Countdown_.SetActive(true);
+    	yield return new WaitForSeconds(1);
+    	Countdown_.SetActive(false);
+    	Countdown_.GetComponent<Text> ().text = "2";
+    	getReady.Play();
+    	Countdown_.SetActive(true);
+    	yield return new WaitForSeconds(1);
+    	Countdown_.SetActive(false);
+    	Countdown_.GetComponent<Text> ().text = "1";
+    	getReady.Play();
+    	Countdown_.SetActive(true);
+    	yield return new WaitForSeconds(1);
+    	Countdown_.SetActive(false);
+    	goAudio.Play();
+    	LapTimer.SetActive(true);
+    }
+    
+}
---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameObject StartTrigg;
+    public GameObject EndTrigg;
+    
+    public GameObject MinuteDisplay;
+    public GameObject SecondDisplay;
+    public GameObject MilliDisplay;
+    
+    public GameObject LapTimeBox;
+    
+    void OnTriggerEnter()
+    {
+    	if(LapTimeManager.SecondCount <= 9)
+    	{
+            SecondDisplay.GetComponent<Text>().text = "0" + LapTimeManager.SecondCount + ".";
+    	}
+    	else
+    	{
+            SecondDisplay.GetComponent<Text>().text = "" + LapTimeManager.SecondCount + ".";
+    	}
+    	
+    	if(LapTimeManager.MinuteCount <= 9)
+    	{
+            MinuteDisplay.GetComponent<Text>().text = "0" + LapTimeManager.MinuteCount + ":";
+    	}
+    	else
+    	{
+            MinuteDisplay.GetComponent<Text>().text = "" + LapTimeManager.MinuteCount + ":";
+    	}
+    	
+    	MilliDisplay.GetComponent<Text>().text = "" + LapTimeManager.MilliCount;
+    	
+    	LapTimeManager.MinuteCount = 0;
+    	LapTimeManager.SecondCount = 0;
+    	LapTimeManager.MilliCount = 0;
+    	
+    	StartTrigg.SetActive(true);
+    	EndTrigg.SetActive(false);
+    }
+}
